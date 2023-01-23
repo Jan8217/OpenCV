70cf0172c2db057260e870055852ef96c3c9cdde
Jan8217 <ge78xus@mytum.de>
2023-01-24 00:11:40 +0100

    second commit 23012023

--- chapter1.py
+++ chapter1.py
@@ -1,14 +1,23 @@
 import cv2
-
-"""
-print("Package Imported")
+import numpy as np
 
 img = cv2.imread("Resources/Lenna_(test_image).png")
+kernel = np.ones((5,5),np.uint8)
 
-cv2.imshow("Output", img)
+imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
+imgCanny = cv2.Canny(img,150,200)
+imgDialation = cv2.dilate(imgCanny,kernel,iterations=1)
+imgEroded = cv2.erode(imgDialation,kernel,iterations=1)
+
+cv2.imshow("Gray Image", imgGray)
+cv2.imshow("Blur Image", imgBlur)
+cv2.imshow("Canny Image", imgCanny)
+cv2.imshow("Dialation Image", imgDialation)
+cv2.imshow("Eroded Image", imgEroded)
+
 cv2.waitKey(0)
 """
-
 cap = cv2.VideoCapture(0)
 cap.set(3,640)
 cap.set(4,480)
@@ -20,4 +29,6 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
 
         break
+
+"""
 
